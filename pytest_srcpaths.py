a1f05b114bf556a38a48baefa9dd05ad93138034
Brian Okken <1568356+okken@users.noreply.github.com>
2021-10-08 09:13:40 -0700

    more closely match behavior of upcoming `pythonpath` feature of pytest.

--- pytest_srcpaths.py
+++ pytest_srcpaths.py
@@ -3,19 +3,30 @@ Add paths to sys.path
 """
 import sys
 
-__version__ = "1.0.6"
+import pytest
 
-def pytest_addoption(parser):
+__version__ = "1.1.0"
+
+def pytest_addoption(parser) -> None:
     parser.addini("srcpaths",
                   type="pathlist",
-                  help="Add paths to sys.path")
+                  help="Add paths to sys.path",
+                  default=[])
 
 
-def pytest_configure(config):
-    paths_from_ini = config.getini("srcpaths")
-    if paths_from_ini:
-        for path in paths_from_ini:
-            sys.path.insert(0, str(path))
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config) -> None:
+    # `pythonpath = a b` will set `sys.path` to `[a, b, x, y, z, ...]`
+    for path in reversed(config.getini("srcpaths")):
+        sys.path.insert(0, str(path))
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_unconfigure(config) -> None:
+    for path in config.getini("srcpaths"):
+        path_str = str(path)
+        if path_str in sys.path:
+            sys.path.remove(path_str)
 
 
 
--- tests/test_srcpaths.py
+++ tests/test_srcpaths.py
@@ -1,77 +1,139 @@
-import pytest
+import sys
 from textwrap import dedent
+from typing import Generator
+from typing import List
+from typing import Optional
+
+import pytest
+from _pytest.pytester import Pytester
 
 
 @pytest.fixture()
-def file_structure(pytester):
-    """
-    test_foo.py
-    test_bar.py
-    sub/foo.py
-    sub2/bar.py
-    """
-    content = dedent(
+def file_structure(pytester: Pytester) -> None:
+    pytester.makepyfile(
+        test_foo="""
+        from foo import foo
+
+        def test_foo():
+            assert foo() == 1
         """
-    from foo import foo
-
-    def test_foo():
-        assert foo() == 1
-    """
     )
-    pytester.makepyfile(test_foo=content)
 
-    content = dedent(
+    pytester.makepyfile(
+        test_bar="""
+        from bar import bar
+
+        def test_bar():
+            assert bar() == 2
         """
-    from bar import bar
-
-    def test_bar():
-        assert bar() == 2
-    """
     )
-    pytester.makepyfile(test_bar=content)
 
-    content = dedent(
-        """
-    def foo():
-        return 1
-    """
-    )
     foo_py = pytester.mkdir("sub") / "foo.py"
+    content = dedent(
+        """
+        def foo():
+            return 1
+        """
+    )
     foo_py.write_text(content, encoding="utf-8")
 
+    bar_py = pytester.mkdir("sub2") / "bar.py"
     content = dedent(
         """
-    def bar():
-        return 2
-    """
+        def bar():
+            return 2
+        """
     )
-    bar_py = pytester.mkdir("sub2") / "bar.py"
     bar_py.write_text(content, encoding="utf-8")
 
 
-def test_one_dir(pytester, file_structure):
+def test_one_dir(pytester: Pytester, file_structure) -> None:
     pytester.makefile(".ini", pytest="[pytest]\nsrcpaths=sub\n")
     result = pytester.runpytest("test_foo.py")
+    assert result.ret == 0
     result.assert_outcomes(passed=1)
 
 
-def test_two_dirs(pytester, file_structure):
+def test_two_dirs(pytester: Pytester, file_structure) -> None:
     pytester.makefile(".ini", pytest="[pytest]\nsrcpaths=sub sub2\n")
     result = pytester.runpytest("test_foo.py", "test_bar.py")
+    assert result.ret == 0
     result.assert_outcomes(passed=2)
 
 
-def test_module_not_found(pytester, file_structure):
-    pytester.makefile(".ini", pytest="[pytest]\n")  # no srcpaths listed
+def test_module_not_found(pytester: Pytester, file_structure) -> None:
+    """Without the srcpaths setting, the module should not be found."""
+    pytester.makefile(".ini", pytest="[pytest]\n")
     result = pytester.runpytest("test_foo.py")
+    assert result.ret == pytest.ExitCode.INTERRUPTED
     result.assert_outcomes(errors=1)
     expected_error = "E   ModuleNotFoundError: No module named 'foo'"
     result.stdout.fnmatch_lines([expected_error])
 
 
-def test_no_ini(pytester, file_structure):
+def test_no_ini(pytester: Pytester, file_structure) -> None:
+    """If no ini file, test should error."""
     result = pytester.runpytest("test_foo.py")
+    assert result.ret == pytest.ExitCode.INTERRUPTED
     result.assert_outcomes(errors=1)
     expected_error = "E   ModuleNotFoundError: No module named 'foo'"
     result.stdout.fnmatch_lines([expected_error])
 
+
+def test_unconfigure_unadded_dir(pytester: Pytester) -> None:
+    """
+    The srcpaths handling adds paths during pytest_load_initial_conftests.
+    This test adds the ini pythopath setting after that, during pytest_configure.
+    Really, any time before unconfigure, would work.
+    Then, when pytest_unconfigure happens, it tries to remove the sub from sys.path,
+    but it isn't there.
+
+    Therefore, the point of this test is to make sure that behavior works, that unconfigure
+    doesn't blow up if a directory isn't in sys.path before unconfigure.
+    """
+    pytester.makeconftest(
+        """
+        def pytest_configure(config):
+          config.addinivalue_line("srcpaths", "sub")
+        """
+    )
+    pytester.makepyfile(
+        """
+        import sys
+
+        def test_something():
+            pass
+        """
+    )
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_clean_up(pytester: Pytester) -> None:
+    """Test that the srcpaths plugin cleans up after itself."""
+    # This is tough to test behaviorly because the cleanup really runs last.
+    # So the test make several implementation assumptions:
+    # - Cleanup is done in pytest_unconfigure().
+    # - Not a hookwrapper.
+    # So we can add a hookwrapper ourselves to test what it does.
+    pytester.makefile(".ini", pytest="[pytest]\nsrcpaths=I_SHALL_BE_REMOVED\n")
+    pytester.makepyfile(test_foo="""def test_foo(): pass""")
+
+    before: Optional[List[str]] = None
+    after: Optional[List[str]] = None
+
+    class Plugin:
+        @pytest.hookimpl(hookwrapper=True, tryfirst=True)
+        def pytest_unconfigure(self) -> Generator[None, None, None]:
+            nonlocal before, after
+            before = sys.path.copy()
+            yield
+            after = sys.path.copy()
+
+    result = pytester.runpytest_inprocess(plugins=[Plugin()])
+    assert result.ret == 0
+
+    assert before is not None
+    assert after is not None
+    assert any("I_SHALL_BE_REMOVED" in entry for entry in before)
+    assert not any("I_SHALL_BE_REMOVED" in entry for entry in after)
